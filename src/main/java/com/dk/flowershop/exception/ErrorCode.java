808cba2eab8720243aed03377a5d3300db5fdcd7
dkulkarni <deepthi.kulkarni@gmail.com>
2016-04-23 20:15:45 +0530

    Ignore build in commits

---
+++ src/main/java/com/dk/flowershop/exception/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.dk.flowershop.exception;
+
+/**
+ * Created by dkulkarni on 23/04/16.
+ */
+public enum ErrorCode {
+
+    INVALID_INPUT
+}
---
+++ src/main/java/com/dk/flowershop/exception/FlowerShopException.java
@@ -0,0 +1,11 @@
+package com.dk.flowershop.exception;
+
+/**
+ * Created by dkulkarni on 23/04/16.
+ */
+public class FlowerShopException extends Exception {
+
+    public FlowerShopException(String message) {
+
+    }
+}
